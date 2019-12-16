22b34e24d2d6b2cfd6edd2dae0a943c49ed884c5
ikayz <ikayz@live.com>
2019-12-16 15:42:22 +0200

    Add object checker matcher

--- functions.js
+++ functions.js
@@ -2,6 +2,11 @@ const functions = {
   add: (num1, num2) => num1 + num2,
   isNull: () => null,
   checkValue: (x) => x,
-}
+  createUser: () => {
+    const user = { firstName: 'Isaac'};
+    user['lastName'] = 'Miti';
+    return user;
+  },
+};
 
 module.exports = functions;
--- functions.test.js
+++ functions.test.js
@@ -10,12 +10,20 @@ test('Adds 2 + 2 equal to not 5', () => {
   expect(functions.add(2, 2)).not.toBe(5);
 });
 
-// Should be null
+// Should be null toBeNull
 test('Should be null', () => {
   expect(functions.isNull()).toBeNull();
 });
 
-// Check falsy values
+// Check falsy values toBeFalsy
 test('Should be a falsy value', () => {
   expect(functions.checkValue(0)).toBeFalsy();
+});
+
+// Check for object
+test('Should be an object', () => {
+  expect(functions.createUser()).toEqual({
+    firstName: 'Isaac',
+    lastName: 'Miti'
+  });
 });
